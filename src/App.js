b8c6518f1978db0aab5236ce1b6be0f170343667
Emma Högberg <emmahogberg88@gmail.com>
2022-09-13 14:15:11 +0200

    Add checkboxes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css'
 import styled from 'styled-components/macro'
 import { useState } from 'react'
+import { passwordList } from './utils/passwordList'
 
 function App() {
 	const [checkedUpper, setCheckedUpper] = useState(false)
@@ -8,7 +9,13 @@ function App() {
 	const [checkedNumber, setCheckedNumber] = useState(false)
 	const [checkedSymbol, setCheckedSymbol] = useState(false)
 
-	const [passwordLength, setPasswordLength] = useState('10')
+	const [isChecked, setIsChecked] = useState(false) //test!
+	const [checkedState, setCheckedState] = useState(new Array(passwordList.length).fill(false))
+
+	const [passwordLength, setPasswordLength] = useState(10)
+	const [password, setPassword] = useState('')
+	const [showText, setShowText] = useState(false)
+	// const [checkedBoxes, setCheckedBoxes] = useState(0)
 
 	const UPPERCASELETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 	const LOWERCASELETTERS = 'abcdefghijklmnopqrstuvwxyz'
@@ -48,12 +55,11 @@ function App() {
 		}
 	}
 
-	const changePasswordLength = (passwordLength) => {
-		console.log(passwordLength)
-		// setPasswordLength(passwordLength)
-	}
+	// console.log(checkedBoxes)
 
 	let selectionOfChars = ''
+	let passwordGenerated = ''
+
 	const generatePassword = () => {
 		if (checkedUpper) {
 			selectionOfChars += UPPERCASELETTERS
@@ -70,13 +76,54 @@ function App() {
 
 		console.log(selectionOfChars)
 
-		for (let i = 1; i < passwordLength; i++) {
-			console.log(i)
+		for (let i = 0; i < passwordLength; i++) {
+			passwordGenerated += selectionOfChars.charAt(Math.floor(Math.random() * selectionOfChars.length))
 		}
-		console.log(Math.floor(Math.random()))
-		//1. kolla vilka boxar som är iklickade
-		//2. om inputvärde ska ingå, lägg till i array eller string (beror på hur jag väljer att göra..)
-		//3. generera ett random lösenord från array/string som har längd = password.length
+		setPassword(passwordGenerated)
+	}
+
+	const revealText = () => {
+		setShowText((reveal) => !reveal)
+	}
+
+	const copyPassword = async (password) => {
+		try {
+			await navigator.clipboard.writeText(password)
+
+			revealText()
+
+			setTimeout(() => {
+				setShowText((reveal) => !reveal)
+			}, 2000)
+		} catch (err) {
+			console.log('error')
+		}
+	}
+
+	const getStrength = (count) => {
+		console.log('hej')
+	}
+
+	const handleChange = (position) => {
+		const updatedState = checkedState.map((item, index) => {
+			return index === position ? !item : item
+		})
+		setCheckedState(updatedState)
+
+		const passwordStrenght = updatedState.filter((value) => value === true)
+
+		if (passwordStrenght.length === 1) {
+			console.log('ett')
+		}
+		if (passwordStrenght.length === 2) {
+			console.log('två')
+		}
+		if (passwordStrenght.length === 3) {
+			console.log('tre')
+		}
+		if (passwordStrenght.length === 4) {
+			console.log('fyra')
+		}
 	}
 
 	return (
@@ -84,14 +131,17 @@ function App() {
 			<HeadingOne>Password Generator</HeadingOne>
 			<InnerWrapper>
 				<Container>
-					<GeneratedPassword>P4$5W0rD!</GeneratedPassword>
-					<Span>
-						<svg width='21' height='24' xmlns='http://www.w3.org/2000/svg'>
+					<GeneratedPassword password={password}>
+						{password.length > 0 ? password : 'P4$5W0rD!'}
+					</GeneratedPassword>
+					<Span onClick={() => copyPassword(password)}>
+						{showText && password ? 'COPIED ' : ''}
+						<DocumentIcon width='21' height='24' xmlns='http://www.w3.org/2000/svg'>
 							<path
 								d='M20.341 3.091 17.909.659A2.25 2.25 0 0 0 16.319 0H8.25A2.25 2.25 0 0 0 6 2.25V4.5H2.25A2.25 2.25 0 0 0 0 6.75v15A2.25 2.25 0 0 0 2.25 24h10.5A2.25 2.25 0 0 0 15 21.75V19.5h3.75A2.25 2.25 0 0 0 21 17.25V4.682a2.25 2.25 0 0 0-.659-1.591ZM12.469 21.75H2.53a.281.281 0 0 1-.281-.281V7.03a.281.281 0 0 1 .281-.281H6v10.5a2.25 2.25 0 0 0 2.25 2.25h4.5v1.969a.282.282 0 0 1-.281.281Zm6-4.5H8.53a.281.281 0 0 1-.281-.281V2.53a.281.281 0 0 1 .281-.281H13.5v4.125c0 .621.504 1.125 1.125 1.125h4.125v9.469a.282.282 0 0 1-.281.281Zm.281-12h-3v-3h.451c.075 0 .147.03.2.082L18.667 4.6a.283.283 0 0 1 .082.199v.451Z'
 								fill='#A4FFAF'
 							/>
-						</svg>
+						</DocumentIcon>
 					</Span>
 				</Container>
 			</InnerWrapper>
@@ -109,7 +159,7 @@ function App() {
 						max={20}
 						step={1}
 						value={passwordLength}
-						onChange={changePasswordLength}
+						onChange={(event) => setPasswordLength(event.target.value)}
 					/>
 
 					<CheckboxContainer>
@@ -179,6 +229,49 @@ function App() {
 						</StyledCheckbox>
 						<Label>Include Symbols</Label>
 					</CheckboxContainer>
+
+					{passwordList.map(({ name, text, count }, index) => {
+						return (
+							<CheckboxWrapper>
+								<label htmlFor={`checkbox-${index}`}>
+									{checkedState[index] && (
+										<CheckboxIcon width='14' height='12' xmlns='http://www.w3.org/2000/svg'>
+											<path stroke='#18171F' strokeWidth='3' fill='none' d='M1 5.607 4.393 9l8-8' />
+										</CheckboxIcon>
+									)}
+									<Checkbox
+										type='checkbox'
+										id={`checkbox-${index}`}
+										name={name}
+										value={name}
+										checked={checkedState[index]}
+										onChange={() => handleChange(index)}
+									/>
+									{text}
+								</label>
+								<p>{checkedState[index] ? 'checked' : 'no'}</p>
+								{getStrength(count)}
+							</CheckboxWrapper>
+						)
+					})}
+
+					{/* <CheckboxWrapper>
+						<label>
+							{isChecked && (
+								<CheckboxIcon width='14' height='12' xmlns='http://www.w3.org/2000/svg'>
+									<path stroke='#18171F' strokeWidth='3' fill='none' d='M1 5.607 4.393 9l8-8' />
+								</CheckboxIcon>
+							)}
+							<Checkbox
+								type='checkbox'
+								checked={isChecked}
+								id='upper'
+								name='upper'
+								onChange={() => setIsChecked((prev) => !prev)}
+							/>
+							testar!
+						</label>
+					</CheckboxWrapper> */}
 				</Form>
 
 				<Container background='var(--clr-bg-primary)' padding='0.6em 1em' margin='1em 0'>
@@ -239,7 +332,7 @@ const HeadingOne = styled.h1`
 `
 
 const GeneratedPassword = styled.h2`
-	color: var(--clr-light);
+	color: ${(props) => (props.password ? 'var(--clr-light)' : 'var(--clr-grey)')};
 	font-size: 1.34rem;
 
 	&:hover,
@@ -258,6 +351,16 @@ const Span = styled.span`
 
 	@media screen and (min-width: 768px) {
 		font-size: 32px;
+	}
+`
+
+const DocumentIcon = styled.svg`
+	cursor: pointer;
+
+	&:hover {
+		path {
+			fill: var(--clr-light);
+		}
 	}
 `
 
@@ -409,5 +512,41 @@ const Arrow = styled.svg`
 		fill: var(--clr-bg-primary);
 	}
 `
+
+//////////////
+const CheckboxIcon = styled.svg`
+	position: absolute;
+	left: 2px;
+	top: 3px;
+`
+
+const Checkbox = styled.input.attrs({ type: 'checkbox' })`
+	-webkit-appearance: none;
+	-moz-appearance: none;
+	appearance: none;
+	width: 18px;
+	height: 18px;
+	border-radius: none;
+	margin-right: 0.5em;
+	border: 2px solid var(--clr-light);
+	outline: none;
+	cursor: pointer;
+	border: 2px solid ${(props) => (props.checked ? 'var(--clr-bg-accent)' : 'var(--clr-light)')};
+	background: ${(props) => (props.checked ? 'var(--clr-accent)' : 'var(--clr-bg-secondary)')};
+
+	// &:focus-within {
+	// 	transform: scale(1.2);
+	// }
+
+	// ${CheckboxIcon} {
+	// 	visibility: ${(props) => (props.checked ? 'visible' : 'hidden')};
+	// }
+`
+
+const CheckboxWrapper = styled.div`
+	position: relative;
+`
+
+///////////////////////
 
 export default App
